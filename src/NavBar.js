0bf3fb0b1775c8d315e7ea1656c3bca5cec53471
snh3003 <shahsama542@gmail.com>
2020-07-01 14:53:43 +0530

    removed extra code

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
         <div>
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="title" color="inherit">
+                <Typography color="inherit">
                 CRUD APP
                 </Typography>
             </Toolbar>
